d38bdf576fd5ea485de96d5e14074704b35b323b
lucas.matricarde <lucas.eduardo@wises.com.br>
2024-11-30 11:08:27 -0300

    Criando parametro model, controller e repository

---
+++ src/main/java/com/lucasmatricarde/sistemavotacaobbb/controller/ParametroController.java
@@ -0,0 +1,35 @@
+package com.lucasmatricarde.sistemavotacaobbb.controller;
+
+import com.lucasmatricarde.sistemavotacaobbb.model.ParametroModel;
+import com.lucasmatricarde.sistemavotacaobbb.repository.ParametroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/parametros")
+public class ParametroController {
+
+    private final ParametroRepository parametroRepository;
+
+    public ParametroController(ParametroRepository parametroRepository) {
+        this.parametroRepository = parametroRepository;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<ParametroModel> save(@RequestBody ParametroModel parametroModel) {
+        ParametroModel savedParametroModel = parametroRepository.save(parametroModel);
+        return ResponseEntity.ok(savedParametroModel);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<ParametroModel> get(@RequestBody String key) {
+        Optional<ParametroModel> optParametroModelFound = parametroRepository.findById(key);
+        if(optParametroModelFound.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optParametroModelFound.get());
+    }
+}
---
+++ src/main/java/com/lucasmatricarde/sistemavotacaobbb/repository/ParametroRepository.java
@@ -0,0 +1,8 @@
+package com.lucasmatricarde.sistemavotacaobbb.repository;
+
+import com.lucasmatricarde.sistemavotacaobbb.model.ParametroModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ParametroRepository extends MongoRepository<ParametroModel, String> {
+
+}
